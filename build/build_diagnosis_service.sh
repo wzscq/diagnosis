219b9b1d4d2a89a79a1192f78c67fdcdba8487bd
wangzhongshu <wzscq@126.com>
2022-07-06 14:11:09 +0800

    fix a bug

--- build/build_diagnosis_service.sh
+++ build/build_diagnosis_service.sh
@@ -7,7 +7,7 @@ if [ ! -e package ]; then
 fi
 
 echo build the code ...
-cd service/diag_service
+cd service/diagnosis_service
 #添加参数CGO_ENABLED=0，关闭CGO,是为了是编译后的程序可以在alpine中运行
 CGO_ENABLED=0 go build
 cd ../..
@@ -20,6 +20,6 @@ if [ -e package/service/diag_service ]; then
   rm -rf package/service/diag_service
 fi
 
-mv ./service/diag_service/diag_service ./package/service/diag_service
+mv service/diagnosis_service/diagnosis ./package/service/diag_service
 
 echo diag_service package build over.
